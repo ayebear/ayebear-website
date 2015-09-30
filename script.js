fec0042a496d3cad8bc26b4d915cb6a6ff4984ec
ayebear <ayebear@gmail.com>
2015-09-30 17:03:49 -0400

    ...

--- script.js
+++
@@ -1,6 +0,0 @@
-function buttonPressed() {
-    var message = document.getElementById("message").value;
-    if (message) {
-        document.getElementById("background").style.background = message;
-    }
-}
